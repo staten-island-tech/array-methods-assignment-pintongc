cbbc38b35c6514a5b282ff8d9138e5bc732d73c8
pintongc <pintongc@nycstudents.net>
2023-10-18 09:17:52 -0400

    2

--- index.js
+++ index.js
@@ -4,41 +4,41 @@
          firstName: "Jack",
          lastName: "Jack",
          graduated: true,
-         grade: 98.2,
+         grades: [98.2, 78, 50, 90],
          age: 18,
      },
      {
          firstName: "John",
          lastName: "John",
          graduated: false,
-         grade: 92.9,
+         grades: [92.9, 14, 95, 78],
          age: 15,
      },
      {
          firstName: "Mel",
          lastName: "Adam",
          graduated: false,
-         grade: 88.5,
+         grades: [88.5, 95, 83, 69],
          age: 14,
      },
      {
         firstName: "Sara",
         lastName: "Mike",
         graduated: true,
-        grade: 78.9,
+        grades: [78.9, 94, 87, 65],
         age: 18,
     },
     {
         firstName: "Michael",
         lastName: "Austin",
         graduated: true,
-        grade: 100,
+        grades: [100, 99, 98, 97],
         age: 18,
     },
     
  ];
 
 students.forEach((students) => console.log(students.firstName));
-students.forEach((students) => console.log(students.grade));
+students.forEach(student => {student.grades.forEach(grade => {console.log(grade)})});
 const graduates = students.filter((student) => student.graduated === true);
 console.log(graduates);
